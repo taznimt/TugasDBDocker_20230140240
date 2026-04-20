5a5b04e72a1bbf578a8d64834e63f1b7eb66290c
taznimt <taznimanwar06@gmail.com>
2026-04-20 10:00:01 +0700

    feat: implement User entity with JPA and Lombok annotations

---
+++ src/main/java/com/praktikumdb/demo2/model/User.java
@@ -0,0 +1,28 @@
+package com.praktikumdb.demo2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "20230140240")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+}
