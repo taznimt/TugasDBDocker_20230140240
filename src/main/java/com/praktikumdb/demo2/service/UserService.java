86f0b3ba09ce5259dcf82a5377664852544f52c5
taznimt <taznimanwar06@gmail.com>
2026-04-20 10:23:25 +0700

    feat: implement UserService with full CRUD operations

---
+++ src/main/java/com/praktikumdb/demo2/service/UserService.java
@@ -0,0 +1,45 @@
+package com.praktikumdb.demo2.service;
+
+
+import com.praktikumdb.demo2.model.User;
+import com.praktikumdb.demo2.repository.UserRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository= userRepository;
+    }
+    public User addUser(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    public User getUSerById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(String id, User request) {
+        User existingUser=userRepository.findById(id).orElse(null);
+        if (existingUser !=null) {
+            existingUser.setName(request.getName());
+            existingUser.setNim(request.getNim());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
